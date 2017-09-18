e1de4072ed14a2f1e862a82d52a4d7d5dd875c71
tony <tony@Sonata>
2017-09-18 07:59:08 +0200

    Main

---
+++ webfmc/WebFMC/src/main/java/com/retroago/fsim/webfmc/WebFmcApp.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.retroago.fsim.webfmc;
+
+/**
+ * @author tony
+ *
+ */
+public final class WebFmcApp {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
